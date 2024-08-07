f27c10838e744575c01aef27024a8fde791e1dfe
Shanel Beebe <shanel.beebe@publicisna.com>
2024-08-07 01:33:20 -0400

    create database query to bring in product data.

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -5,54 +5,46 @@ import SelectSize from "./SelectSize";
 
 /* eslint-disable react/prop-types */
 export default function GalleryItem(props) {
-    const { shoe, category, itemOrder, setItemOrder, setOrderData } = props;
-    const [quantity, setQuantity] = useState(0);
-    const [selectedSize, setSelectedSize] = useState("6");
+  const { shoe, category, setItemOrder, addToBag } = props;
+  const [quantity, setQuantity] = useState(0);
+  const [selectedSize, setSelectedSize] = useState("6");
 
-    const addToBag = () => {
-        setItemOrder([shoe, quantity, selectedSize]);
-        setOrderData(orderData => [...orderData, itemOrder]);
-        console.log(itemOrder);
-    };
+  const updateItem = () => {
+    setItemOrder([{ shoe, quantity, selectedSize }]);
+    addToBag();
+  };
 
-    return (
-        <>
-            {shoe.category === category ||
-            (shoe.newArrival === true && category === "new arrivals") ? (
-                <div className="galleryItem">
-                    <p className="brand">{shoe.brand}</p>
-                    <p className="style">{shoe.style}</p>
-                    <Image
-                        className="shoeImg"
-                        src={shoe.image}
-                        alt={shoe.style}
-                        width={200}
-                        height={200}
-                        style={{
-                            width: "100%",
-                            height: "auto",
-                            cursor: "pointer",
-                            objectFit: "cover",
-                        }}
-                    />
-                    <p className="price">${shoe.price}</p>
-                    <div className="galleryItemInputs">
-                        <SelectQuantity
-                            quantity={quantity}
-                            setQuantity={setQuantity}
-                        ></SelectQuantity>
-                        <SelectSize
-                            selectedSize={selectedSize}
-                            setSelectedSize={setSelectedSize}
-                        ></SelectSize>
-                    </div>
+  return (
+    <>
+      {shoe.category === category || (shoe.new_arrival === true && category === "new arrivals") ? (
+        <div className='galleryItem'>
+          <p className='brand'>{shoe.brand}</p>
+          <p className='style'>{shoe.style}</p>
+          <Image
+            className='shoeImg'
+            src={shoe.image_url}
+            alt={shoe.style}
+            width={200}
+            height={200}
+            style={{
+              width: "100%",
+              height: "auto",
+              cursor: "pointer",
+              objectFit: "cover",
+            }}
+          />
+          <p className='price'>${shoe.price}</p>
+          <div className='galleryItemInputs'>
+            <SelectQuantity quantity={quantity} setQuantity={setQuantity}></SelectQuantity>
+            <SelectSize selectedSize={selectedSize} setSelectedSize={setSelectedSize}></SelectSize>
+          </div>
 
-                    <button className="addToBag galleryAdd" onClick={addToBag}>
-                        Add To Bag
-                    </button>
-                </div>
-            ) : null}
-        </>
-    );
+          <button className='addToBag galleryAdd' onClick={updateItem}>
+            Add To Bag
+          </button>
+        </div>
+      ) : null}
+    </>
+  );
 }
 /* eslint-enable react/prop-types */
--- src/components/ShoppingBag.js
+++ src/components/ShoppingBag.js
@@ -1,46 +1,47 @@
-export default function ShoppingBag(props) {
-    const { orderData, css } = props;
+import { useEffect } from "react";
 
-    return (
-        <div className="shoppingBag" style={css}>
-            <h4>shoos.</h4>
-            <div className="shoppingContent">
-                <h5>Shopping Bag</h5>
-                {orderData ? (
-                    orderData.map((shoe, index) => {
-                        return (
-                            <div className="shoppingContentItem" key={index}>
-                                <img src={shoe.image} alt="" />
-                                <div>
-                                    <p className="brand">{shoe.brand}</p>
-                                    <p className="style">{shoe.style}</p>
-                                    <p className="size">Size: {shoe.size}</p>
-                                </div>
-                                <div>
-                                    <p className="qty">Qty</p>
-                                    <p className="price">Price</p>
-                                </div>
-                                <div>
-                                    <p className="qty qtyNum">
-                                        {shoe.quantity}
-                                    </p>
-                                    <p className="price">{shoe.price}</p>
-                                </div>
-                            </div>
-                        );
-                    })
-                ) : (
-                    <p>Your Shopping Bag is Empty</p>
-                )}
-                <div className="total">
-                    <p>Total:</p>
-                    <p>$100.00</p>
+/* eslint-disable react/prop-types */
+export default function ShoppingBag(props) {
+  const { orderData, css } = props;
+
+  return (
+    <div className='shoppingBag' style={css}>
+      <h4>shoos.</h4>
+      <div className='shoppingContent'>
+        <h5>Shopping Bag</h5>
+        {orderData ? (
+          orderData.map((shoe, index) => {
+            return (
+              <div className='shoppingContentItem' key={index}>
+                <img src={shoe.src} alt='' />
+                <div>
+                  <p className='brand'>{shoe.brand}</p>
+                  <p className='style'>{shoe.style}</p>
+                  <p className='size'>Size: {shoe}</p>
                 </div>
-            </div>
-            <div className="shoppingBtns">
-                <button>Checkout</button>
-                <button>Continue Shopping</button>
-            </div>
+                <div>
+                  <p className='qty'>Qty</p>
+                  <p className='price'>Price</p>
+                </div>
+                <div>
+                  <p className='qty qtyNum'>{shoe.quantity}</p>
+                  <p className='price'>{shoe.price}</p>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <p>Your Shopping Bag is Empty</p>
+        )}
+        <div className='total'>
+          <p>Total:</p>
+          <p>$100.00</p>
         </div>
-    );
+      </div>
+      <div className='shoppingBtns'>
+        <button>Checkout</button>
+        <button>Continue Shopping</button>
+      </div>
+    </div>
+  );
 }
